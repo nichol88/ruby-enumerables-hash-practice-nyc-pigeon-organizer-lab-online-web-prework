e4a4b36f1ca6e348b2d8289d0170c0935359308d
Brian Nicholls <nichol88@gmail.com>
2019-11-07 05:11:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,7 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  #puts "Input data:"
-  #puts pp(data)
-  # HoHoA
   pigeon_list = {}
 
-
   data.each { |hash_title, attributes|
-    puts "Titles: #{hash_title}, attributes: #{attributes}"
-
     attributes.each { |attribute, names|
       names.each { |name|
         if !pigeon_list[name]
@@ -18,12 +11,7 @@ def nyc_pigeon_organizer(data)
       }
     }
   }
-
-
-
-
   pigeon_list
-
 end
 
 def add_pigeon(name)
@@ -33,40 +21,3 @@ def add_pigeon(name)
     :lives => []
   }
 end
-
-
-=begin
-
-colors = data[:color]
-genders = data[:gender]
-lives = data[:lives]
-
-
-  colors.each { |color, names|
-    names.each { |name|
-      if !pigeon_list[name]
-        pigeon_list[name] = add_pigeon(name)
-      end
-      pigeon_list[name][:color].push(color.to_s)
-    }
-  }
-
-  genders.each { |gender, names|
-    names.each { |name|
-      if !pigeon_list[name]
-        pigeon_list[name] = add_pigeon(name)
-      end
-      pigeon_list[name][:gender].push(gender.to_s)
-    }
-  }
-
-  lives.each { |location, names|
-    names.each { |name|
-      if !pigeon_list[name]
-        pigeon_list[name] = add_pigeon(name)
-      end
-      pigeon_list[name][:lives].push(location.to_s)
-    }
-  }
-
-=end
