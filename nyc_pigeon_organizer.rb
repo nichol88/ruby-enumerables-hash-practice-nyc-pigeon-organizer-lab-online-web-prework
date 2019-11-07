2e2868e709594e9a05e575018707cd7450f4021c
Brian Nicholls <nichol88@gmail.com>
2019-11-07 05:10:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,7 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  #puts "Input data:"
-  #puts pp(data)
-  # HoHoA
   pigeon_list = {}
 
-
   data.each { |hash_title, attributes|
-    puts "Titles: #{hash_title}, attributes: #{attributes}"
-
     attributes.each { |attribute, names|
       names.each { |name|
         if !pigeon_list[name]
@@ -18,12 +11,7 @@ def nyc_pigeon_organizer(data)
       }
     }
   }
-
-
-
-
   pigeon_list
-
 end
 
 def add_pigeon(name)
